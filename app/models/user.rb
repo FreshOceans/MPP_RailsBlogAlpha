5e2b1d024d572b455580a5991480a251a6e7bc1f
FreshOceans <mperezz398@gmail.com>
2017-07-11 17:40:49 -0400

    end of day commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,4 @@ class User < ApplicationRecord
     has_one :address, dependent: :destroy
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
-
-    # validates :username, :uniqueness => true, :presence => { :message => "Username cannot be blank" }
-    # validates :password, :presence => { :message => "Please enter a password that contains at least 10 letters including at least one number and a special character" }, :confirmation => true
-    # validates :email, :presence => { :message => "Please enter a valid email" }, :confirmation => true
-
 end

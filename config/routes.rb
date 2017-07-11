427de5af2877097005286d95d1eacb160975006f
FreshOceans <mperezz398@gmail.com>
2017-07-11 16:15:43 -0400

    fixed login fields, registration fields

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # route format for customized controllers with devise functionality
-  # devise_for :users, :controllers => { registrations: 'registrations' }
+  # devise_for :users, :controllers => { :registrations => 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   # == devise routes
-  devise_for :users
+  # devise_for :users
 
   # == custom routes
   # get "/" => "home#home"
@@ -15,8 +16,6 @@ Rails.application.routes.draw do
   # get "/login_form" => "users#login_form"
   # post "/login" => "users#login"
   # get "/logout" => "users#logout"
-
-
 
   # == RESTful routes
   resources :users do
